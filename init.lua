ce88edad7ff38b798513b6851be7cc96deb82d32
ZachStecko <zach.stecko@gmail.com>
2024-12-11 22:34:59 -0500

    terminal, lazygit
    foobar

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -927,9 +927,9 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
@@ -938,7 +938,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
@@ -967,4 +967,5 @@ require('lazy').setup({
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+-- vim: ts=2 sts=2 sw=2 eot
+-- vim.o.laststatus = 3
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,3 +1,12 @@
+--[[
+  Plugin checklist
+  - icons
+  - file explorer
+  - terminal
+  - harpoon
+  - lazy git
+  ]]
+
 return {
   -- nvim-web-devicons for file icons
   {
@@ -21,5 +30,60 @@ return {
       -- Keybinding to toggle the file explorer
       vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Toggle File Explorer' })
     end,
+  },
+  -- { 'akinsho/toggleterm.nvim', version = '*', config = true },
+  -- or
+  --{'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
+  -- harpoon
+  -- terminal
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {
+      -- Keybindings
+      open_mapping = '<leader>t', -- Map leader key + 't' to open a terminal
+      size = 20, -- Set the initial terminal size
+      hide_numbers = true, -- Hide the terminal numbers
+      shade_terminals = true, -- Shade the terminals
+      start_in_insert = true, -- Start the terminal in insert mode
+      insert_mappings = true, -- Enable insert mode mappings
+      persist_mode = true, -- Persist the terminal mode
+      shell_command = 'term', -- Set the shell command to use
+
+      -- Vertical split settings
+      vertical = {
+        size = 30, -- Set the vertical split size
+        orientation = 'right', -- Set the orientation to right
+      },
+
+      -- Horizontal split settings
+      horizontal = {
+        size = 30, -- Set the horizontal split size
+        orientation = 'bottom', -- Set the orientation to bottom
+      },
+    },
+  },
+  {
+    'nvim-lua/plenary.nvim',
+  },
+  {
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
   },
 }

0b8756bc585f5a5f1f4c022e58d7269da584d135
ZachStecko <zach.stecko@gmail.com>
2024-12-11 22:50:24 -0500

    syntax
    syntax

--- init.lua
+++ init.lua
@@ -967,5 +967,4 @@ require('lazy').setup({
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 eot
--- vim.o.laststatus = 3
+-- vim: ts=2 sts=2 sw=2 et
